42cafa0db1c3995cf24dab23ed2e168ab146b951
thaideptraihihi <83362818+thaideptraihihi@users.noreply.github.com>
2022-04-19 23:43:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Anh có điều này muốn nói với em
+    Em phải trả lời thật lòng nha`,
     btnIntro: 'hihi',
-    title: '7 năm nữa anh sẽ cầu hôn em 😙',
-    desc: 'Lúc đó em sẽ đồng ý làm vợ anh chứ ? ',
+    title: 'Anh thích em😙',
+    desc: 'EM có đồng ý làm nguoiiu anh không ? ',
     btnYes: 'Dạ en đồng ý <3',
-    btnNo: 'Không, chui dô chậu cây ik :3',
+    btnNo: 'Không, em chỉ coi anh như là một người anh trai thôi :)) :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tại anh đẹp traiiiiii VL  <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 10h30 anh qua đưa trà sữa rồi hun em nhé công chúa của anhhh.',   
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.messenger.com/t/100010176637008' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.messenger.com/t/100014108268323' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
